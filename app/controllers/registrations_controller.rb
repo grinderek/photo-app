0c43355c3ded2bddcd4fd6c3a3a519dc719ac8f6
Danila Berazhny <dannmmaster@gmail.com>
2021-11-18 17:22:04 +0300

    Add stripe and production deploy

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,14 +1,14 @@
-
 class RegistrationsController < Devise::RegistrationsController
   def create
     build_resource(sign_up_params)
+    byebug
 
     resource.class.transaction do
       resource.save
       yield resource if block_given?
       if resource.persisted?
         @payment = Payment.new({ email: params["user"]["email"],
-                                 token: params[:payment]["token"], user_id: resource.id })
+                                 token: params["stripeToken"], user_id: resource.id })
 
         flash[:error] = "Please check registration errors" unless @payment.valid?
 
